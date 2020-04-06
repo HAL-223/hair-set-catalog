d8902433a829c7537de52d4c07b62eb61efe426f
HAL-223 <anoble.ldy8@gmail.com>
2020-04-06 12:33:56 +0900

    エラー表示されない

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
-<?php 
+<?php
 require_once('config.php');
 require_once('functions.php');
 
 // データベース接続
 $dbh = connectDb();
 
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ヘアセットカタログHOME</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="flex-col-area">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+      <a href="http://localhost/19_blog_system/index.php" class="navbar-brand">Hair set catalog</a>
+    </nav>
+
+    
+
+    <footer class="footer font-small bg-dark">
+      <div class="footer-copyright text-center py-3 text-light">&copy; HAL hair</div>
+    </footer>
+  </div>
+</body>
+
+</html>
